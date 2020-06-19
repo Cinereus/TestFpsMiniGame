ce3a5cb80ac679d911396f7131b51d54baa9ad18
DeadlySpinach <9featherbrain6@gmail.com>
2020-06-19 17:31:04 +0600

    Gun and bullet scripts, and bulletStats scriptable object added

---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    [SerializeField] private GameObject _bullet;
+    
+    private Camera _camera;
+    private float rotationX;
+    private float rotationY;
+    
+    private void Awake()
+    {
+        _camera = Camera.main;
+    }
+    
+    private void Update()
+    { 
+        var isButtonPressed = Input.GetMouseButtonDown(0);
+        var mousePos = _camera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10f));
+
+        rotationX -= mousePos.y/10f;
+        rotationY += mousePos.x/10f;
+
+        rotationX = Mathf.Clamp(rotationX, -5f, 5f);
+        rotationY = Mathf.Clamp(rotationY, -5, 5f);
+
+        transform.localRotation = Quaternion.Euler(rotationX, rotationY, 0f);
+
+        if (isButtonPressed)
+        {
+            var bullet = Instantiate(_bullet).GetComponent<Bullet>();
+            var position = transform.position;
+            
+            bullet.gameObject.SetActive(true);
+            bullet.transform.position = new Vector3(position.x, position.y, position.z+0.2f);
+            bullet.Move(new Vector3(mousePos.x, mousePos.y, mousePos.z));
+        }
+    }
+}
---
+++ Assets/Scripts/ScriptableObjects/BulletStats.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "BulletStat", fileName = "New Bullet Stat")]
+public class BulletStats : ScriptableObject
+{
+    [Range(0, 1)] public float Speed = 0.1f;
+    public float BlowStrength;
+}
